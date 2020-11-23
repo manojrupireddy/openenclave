b1593a84499fda083bcdd23118eb1481a121adeb
manoj rupireddy <marupire@microsoft.com>
2020-11-23 19:14:00 +0000

    removing compile defined flags and adding client/server specific claim verification functions
    Signed-off-by: manoj rupireddy <marupire@microsoft.com>

---
+++ samples/attested_tls/server/enc/cert_verify_config.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+#include "cert_verify_config.h"
+
+oe_result_t verify_claim_value(const oe_claim_t* claim)
+{
+    oe_result_t result = OE_OK;
+    printf("\nverify unique_id:\n");
+    for (size_t i = 0; i < claim->value_size; i++)
+        printf("0x%0x ", (uint8_t)claim->value[i]);
+    printf("\n");
+    return result;
+}
